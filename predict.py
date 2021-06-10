20eba1441ad4490f73b596eb1120a8fefcb7fd80
liao2000 <liao2000@livemail.tw>
2021-06-10 21:56:47 +0800

    Simplify

--- predict.py
+++ predict.py
@@ -2,22 +2,7 @@ import librosa
 import numpy as np
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
-import math
 import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.optimizers import Adam
-
-input_shape = (128, 33)
-
-model = Sequential()
-model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=input_shape))
-model.add(LSTM(units=32,  dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
-model.add(Dense(units=9, activation="softmax"))
-model.compile(loss="categorical_crossentropy", optimizer=Adam(learning_rate=1e-5), metrics=["accuracy"])
-
-def load_model(path):
-    model.load_weights(path)
 
 genre_list = [
     "blues",
@@ -34,7 +19,6 @@ genre_list = [
 def get_x(path):
     data = np.zeros((1, (128), 33), dtype=np.float64)
     hop_length = 512
-    print(f'path = {path}')
     y, sr = librosa.load(path)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
     spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=hop_length)
@@ -47,10 +31,11 @@ def get_x(path):
 
     return data
 
-def get_predict(path):
-    x = get_x(path)
+def get_predict(model_path, music_path):
+    model = tf.keras.models.load_model(model_path)
+    x = get_x(music_path)
     y = model.predict(x)
     print(f'y = {y}')
     r = [float(y[0, i] * 100) for i in range(9)]
-    data = {'filename': path.split('/')[-1], 'gernes': r}
+    data = {'filename': music_path, 'gernes': r}
     return data
