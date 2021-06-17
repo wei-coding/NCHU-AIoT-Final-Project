2d021373b73ff9067a79a6f6e62af1dc82613c8b
wei-coding <edward.yu0830@gmail.com>
2021-06-17 17:09:28 +0800

    heroku

--- predict.py
+++ predict.py
@@ -1,11 +1,11 @@
-import librosa
+# import librosa
 import numpy as np
-import os
 import random
+# import os
 import time
 import json
-os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
-import tensorflow as tf
+# os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
+# import tensorflow as tf
 # import simpleaudio as sa
 
 moods = ['scary', 'happy', 'sad', 'angry', 'No']
@@ -24,27 +24,27 @@ mood_prob = {'total': 5}
 given_mood_prob = {}
 random.seed(time.time())
 
-def get_x(path):
-    data = np.zeros((1, (128), 33), dtype=np.float64)
-    hop_length = 512
-    y, sr = librosa.load(path)
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
-    spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=hop_length)
-    chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length)
-    spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr, hop_length=hop_length)
-    data[0, :, 0:13] = mfcc.T[0:128, :]
-    data[0, :, 13:14] = spectral_center.T[0:128, :]
-    data[0, :, 14:26] = chroma.T[0:128, :]
-    data[0, :, 26:33] = spectral_contrast.T[0:128, :]
+# def get_x(path):
+#     data = np.zeros((1, (128), 33), dtype=np.float64)
+#     hop_length = 512
+#     y, sr = librosa.load(path)
+#     mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=hop_length, n_mfcc=13)
+#     spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=hop_length)
+#     chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length)
+#     spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr, hop_length=hop_length)
+#     data[0, :, 0:13] = mfcc.T[0:128, :]
+#     data[0, :, 13:14] = spectral_center.T[0:128, :]
+#     data[0, :, 14:26] = chroma.T[0:128, :]
+#     data[0, :, 26:33] = spectral_contrast.T[0:128, :]
 
-    return data
+#     return data
 
-def get_predict(model_path, music_path):
-    model = tf.keras.models.load_model(model_path)
-    x = get_x(music_path)
-    y = model.predict(x)
-    print(f'y = {y}')
-    return [float(y[0, i] * 100) for i in range(9)]
+# def get_predict(model_path, music_path):
+#     model = tf.keras.models.load_model(model_path)
+#     x = get_x(music_path)
+#     y = model.predict(x)
+#     print(f'y = {y}')
+#     return [float(y[0, i] * 100) for i in range(9)]
 
 def init_porb():
     for mood in moods:
